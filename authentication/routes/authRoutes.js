c18d51930ca9d05578ecef53c548187120ed1029
Titus210 <tituskiplagat210@gmail.com>
2024-10-19 00:56:33 +0300

    - spacing view

--- authentication/routes/authRoutes.js
+++ authentication/routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-
 // Define routes and map them to controller methods
 router.post('/login', authController.login);
 router.post('/register', authController.register);
