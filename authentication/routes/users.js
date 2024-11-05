078b918ca139beb667095121f2c5dad6da803024
Titus210 <tituskiplagat210@gmail.com>
2024-11-06 02:15:47 +0300

    - implemented adding user profile to database

--- authentication/routes/users.js
+++ authentication/routes/users.js
@@ -1,34 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../config/database');
+const userController = require('../controllers/userController');
 
 // Create a new user
-router.post('/', async (req, res) => {
-    const { email, password, role } = req.body;
-    console.log(req.body);
-    if (!email || !password || !role) {
-        return res.status(400).json({ message: 'All fields are required' });
-    }
-
-    try {
-        const [existingUser] = await pool.execute('SELECT * FROM users WHERE email = ?', [email]);
-
-        if (existingUser.length > 0) {
-            return res.status(400).json({ message: 'User already exists' });
-        }
-
-        const [result] = await pool.execute(
-            'INSERT INTO users (email, password_hash, role) VALUES (?, ?, ?)',
-            [email, password, role]
-        );
-
-        res.status(201).json({ id: result.insertId, email, password, role, message: 'User created successfully' });
- 
-         } catch (error) {
-             console.error(error);
-             res.status(500).json({ message: 'Server error' });
-    }
-});
+router.post('/add-user', userController.addUser);
 
 // Read all users
 router.get('/', async (req, res) => {
@@ -40,5 +16,11 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 });
+
+
+router.get('/:email', userController.getUserId);
+router.post('/profile',userController.addUserProfile);
+
+
 
 module.exports = router;
