154476735430d954ab21cc30bb353f83ee4ec8ab
Titus210 <tituskiplagat210@gmail.com>
2024-11-06 01:20:07 +0300

    - first implementation of adding user to database

---
+++ authentication/routes/users.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/database');
+
+// Create a new user
+router.post('/', async (req, res) => {
+    const { email, password, role } = req.body;
+    console.log(req.body);
+    if (!email || !password || !role) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    try {
+        const [existingUser] = await pool.execute('SELECT * FROM users WHERE email = ?', [email]);
+
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const [result] = await pool.execute(
+            'INSERT INTO users (email, password_hash, role) VALUES (?, ?, ?)',
+            [email, password, role]
+        );
+
+        res.status(201).json({ id: result.insertId, email, password, role, message: 'User created successfully' });
+ 
+         } catch (error) {
+             console.error(error);
+             res.status(500).json({ message: 'Server error' });
+    }
+});
+
+
+module.exports = router;
