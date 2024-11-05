40c81126ab95b2874cebeee9f9c987c3950ad9c8
Titus210 <tituskiplagat210@gmail.com>
2024-11-06 01:04:32 +0300

    - database configurations to use mysql database

--- authentication/config/database.js
+++ authentication/config/database.js
@@ -1,9 +1,15 @@
-const { Sequelize } = require('sequelize');
+const mysql = require('mysql2/promise');
+require('dotenv').config({ path: '../.env' });
 
-// Database configuration
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-module.exports = sequelize;
+module.exports = pool;
